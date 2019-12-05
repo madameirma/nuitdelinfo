3feba1bb39c361334dd4277c107d0fa79d3a66a6
Etidou <45783736+Etidou@users.noreply.github.com>
2019-12-05 19:57:26 +0100

    ok

--- www/js/index.js
+++ www/js/index.js
@@ -3,8 +3,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("header").style.fontSize = "20px";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("header").style.fontSize = "60px";
   }
 }
